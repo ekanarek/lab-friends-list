c970b65ccd3f591d4a9ad984ecd14103a7520297
Emily Kanarek <emilyakanarek@gmail.com>
2024-06-18 11:04:26 -0400

    Added HTML elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
 export default function App() {
-  return <div>Hello</div>;
+  return (<div>
+    <label htmlFor="picture">Picture:</label>
+    <input type="text" id="picture" />
+
+    <label htmlfor="name">Name:</label>
+    <input type="text" id="name" />
+    
+    <button type="button">Add Friend</button>
+  </div>);
 }
