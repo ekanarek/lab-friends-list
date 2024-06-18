cf244f7e1e8a44fab8a24ad905963888476ac6b6
Emily Kanarek <emilyakanarek@gmail.com>
2024-06-18 11:12:27 -0400

    Set up useState hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
+
 export default function App() {
+  const [friends, setFriends] = useState([]);
+  const [picture, setPicture] = useState('');
+  const [name, setName] = useState('');
+  
   return (<div>
     <label htmlFor="picture">Picture:</label>
-    <input type="text" id="picture" />
+    <input 
+      type="text" 
+      id="picture" 
+      value={picture} 
+      onChange={(e) => setPicture(e.target.value)} 
+    />
 
-    <label htmlfor="name">Name:</label>
-    <input type="text" id="name" />
-    
+    <label htmlFor="name">Name:</label>
+    <input 
+      type="text" 
+      id="name"
+      value={name}
+      onChange={(e) => setName(e.target.value)} 
+    />
+
     <button type="button">Add Friend</button>
   </div>);
 }
